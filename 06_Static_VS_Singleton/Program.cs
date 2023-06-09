e9df8b83ac1ba0c80656be7980e52f9c5c2f9ac7
Toseef Ahmad <Toseef.Ahmad@cireson.com>
2023-06-10 00:21:42 +0500

    Add project files.

---
+++ 06_Static_VS_Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _06_Static_VS_Singleton;
+
+Console.WriteLine("Hello, World!");
+
+
+
+double celcius = 37;
+double fahrenhiet = 98.6;
+
+Console.WriteLine("{0} Fahrenhiet converted to Celcius {1}", fahrenhiet, StaticDemo.toCelcius(fahrenhiet));
+Console.WriteLine("{0} Celcius converted to Fahrenhiet {1}", celcius, StaticDemo.toFahrenheit(celcius));
---
+++ 06_Static_VS_Singleton/StaticDemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Static_VS_Singleton
+{
+    internal static class StaticDemo
+    {
+        public static double toFahrenheit(double celcius)
+        {
+            return (celcius * 9 / 5) + 32;
+        }
+        public static double toCelcius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5/9;
+        }
+    }
+}
